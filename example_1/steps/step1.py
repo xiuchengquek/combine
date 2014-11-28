82b589d7206c91958e0cca7e48350ce0cf72faf5
xiuchengquek <x.quek@garvan.org.au>
2014-11-28 13:55:37 +1100

    CHange

--- example_1/steps/step1.py
+++ example_1/steps/step1.py
@@ -1,19 +1,20 @@
-
 from behave import *
+
 def addition(a, b):
 	return a + b
-@given(u'I have entered {value_a} into the calculator')
-def step_impl(context, value_a):
-    assert int(value_a) , 50
 
-@given(u'I added another {value_b} into the calculator')
-def step_impl(context, value_b):
-    context.b = int(value_b)
+
+@given(u'I have entered 50 into the calculator')
+def step_impl(context):
+    pass
+@given(u'I added another 70 into the calculator')
+def step_impl(context):
+    pass
 
 @when(u'I press add')
 def step_impl(context):
     pass
 
-@then(u'the results should be {value_results} on the screen')
-def step_impl(context, value_results):
-    assert addition(context.a, context.b) ==  int(value_results)
+@then(u'the results should be 120 on the screen')
+def step_impl(context):
+    assert addition(50, 70) == 120
